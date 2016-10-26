ad7f4e6222345ff20e375b41176a357a727857b0
Kan Ouivirach <kan@prontomarketing.com>
2016-10-26 11:31:37 +0700

    Introduce initial feedback page

---
+++ sentiment_analyzer/feedback/tests/test_views.py
@@ -0,0 +1,13 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+
+class FeedbackViewTest(TestCase):
+    def setUp(self):
+        self.url = reverse('feedback')
+
+    def test_access_feedback_index_page(self):
+        response = self.client.get(self.url)
+
+        expected = '<h1>Feedback</h1>'
+        self.assertContains(response, expected, status_code=200)
--- sentiment_analyzer/feedback/views.py
+++ sentiment_analyzer/feedback/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class FeedbackView(TemplateView):
+    template_name = 'feedback.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
