3172a842beb9391ddb284224b04715a64c215c55
Kan Ouivirach <kan@prontomarketing.com>
2016-10-26 20:37:09 +0700

    Add sentiment choices in feedback

--- sentiment_analyzer/feedback/models.py
+++ sentiment_analyzer/feedback/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 
 class Feedback(models.Model):
+    SENTIMENT_CHOICES = (
+        ('positive', 'Positive'),
+        ('neutral', 'Neutral'),
+        ('negative', 'Negative'),
+    )
+
     message = models.TextField(blank=True)
--- sentiment_analyzer/feedback/tests/test_models.py
+++ sentiment_analyzer/feedback/tests/test_models.py
@@ -4,12 +4,24 @@ from ..models import Feedback
 
 
 class FeedbackTest(TestCase):
+    def setUp(self):
+        self.feedback = Feedback()
+
     def test_save_feedback_should_have_data_in_each_defined_field(self):
-        feedback = Feedback()
-        feedback.message = "I'm happy"
-        feedback.save()
+        self.feedback.message = "I'm happy"
+        self.feedback.save()
 
         feedback = Feedback.objects.latest('id')
 
         expected = "I'm happy"
         self.assertEqual(feedback.message, expected)
+
+    def test_question_should_have_metric_choices(self):
+        actual = self.feedback.SENTIMENT_CHOICES
+
+        expected = (
+            ('positive', 'Positive'),
+            ('neutral', 'Neutral'),
+            ('negative', 'Negative'),
+        )
+        self.assertEqual(actual, expected)
